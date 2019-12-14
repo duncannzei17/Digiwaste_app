618df4fc63f4ba07b8ca1f3c6d16bc4a2dddc967
unknown <joyperis11@gmail.com>
2019-12-14 17:05:17 +0300

    removed overflow in signup

--- lib/SignUp/signUpScreen.dart
+++ lib/SignUp/signUpScreen.dart
@@ -47,170 +47,177 @@
               Positioned(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Card(
-                        elevation: 4.0,
-                        color: Colors.white,
-                        margin: EdgeInsets.only(left: 20, right: 20),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              /////////////// name////////////
+                  child: Center(
+                    child: ListView(
 
-                              TextField(
-                                style: TextStyle(color: Color(0xFF000000)),
-                                controller: firstNameController,
-                                cursorColor: Color(0xFF9b9b9b),
-                                keyboardType: TextInputType.text,
-                                decoration: InputDecoration(
-                                  prefixIcon: Icon(
-                                    Icons.account_circle,
-                                    color: Colors.grey,
+
+                      children: <Widget>[
+
+                        SizedBox(height: 100,),
+                        Card(
+                          elevation: 4.0,
+                          color: Colors.white,
+                          margin: EdgeInsets.only(left: 20, right: 20),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                /////////////// name////////////
+
+                                TextField(
+                                  style: TextStyle(color: Color(0xFF000000)),
+                                  controller: firstNameController,
+                                  cursorColor: Color(0xFF9b9b9b),
+                                  keyboardType: TextInputType.text,
+                                  decoration: InputDecoration(
+                                    prefixIcon: Icon(
+                                      Icons.account_circle,
+                                      color: Colors.grey,
+                                    ),
+                                    hintText: "Firstname",
+                                    hintStyle: TextStyle(
+                                        color: Color(0xFF9b9b9b),
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal),
                                   ),
-                                  hintText: "Firstname",
-                                  hintStyle: TextStyle(
-                                      color: Color(0xFF9b9b9b),
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.normal),
                                 ),
-                              ),
-                              TextField(
-                                style: TextStyle(color: Color(0xFF000000)),
-                                controller: lastNameController,
-                                cursorColor: Color(0xFF9b9b9b),
-                                keyboardType: TextInputType.text,
-                                decoration: InputDecoration(
-                                  prefixIcon: Icon(
-                                    Icons.account_circle,
-                                    color: Colors.grey,
+                                TextField(
+                                  style: TextStyle(color: Color(0xFF000000)),
+                                  controller: lastNameController,
+                                  cursorColor: Color(0xFF9b9b9b),
+                                  keyboardType: TextInputType.text,
+                                  decoration: InputDecoration(
+                                    prefixIcon: Icon(
+                                      Icons.account_circle,
+                                      color: Colors.grey,
+                                    ),
+                                    hintText: "Lastname",
+                                    hintStyle: TextStyle(
+                                        color: Color(0xFF9b9b9b),
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal),
                                   ),
-                                  hintText: "Lastname",
-                                  hintStyle: TextStyle(
-                                      color: Color(0xFF9b9b9b),
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.normal),
                                 ),
-                              ),
 
-                              /////////////// Email ////////////
+                                /////////////// Email ////////////
 
-                              TextField(
-                                style: TextStyle(color: Color(0xFF000000)),
-                                controller: mailController,
-                                cursorColor: Color(0xFF9b9b9b),
-                                keyboardType: TextInputType.text,
-                                decoration: InputDecoration(
-                                  prefixIcon: Icon(
-                                    Icons.mail,
-                                    color: Colors.grey,
+                                TextField(
+                                  style: TextStyle(color: Color(0xFF000000)),
+                                  controller: mailController,
+                                  cursorColor: Color(0xFF9b9b9b),
+                                  keyboardType: TextInputType.text,
+                                  decoration: InputDecoration(
+                                    prefixIcon: Icon(
+                                      Icons.mail,
+                                      color: Colors.grey,
+                                    ),
+                                    hintText: "Email ",
+                                    hintStyle: TextStyle(
+                                        color: Color(0xFF9b9b9b),
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal),
                                   ),
-                                  hintText: "Email ",
-                                  hintStyle: TextStyle(
-                                      color: Color(0xFF9b9b9b),
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.normal),
                                 ),
-                              ),
 
-                              /////////////// password ////////////
+                                /////////////// password ////////////
 
-                              TextField(
-                                style: TextStyle(color: Color(0xFF000000)),
-                                cursorColor: Color(0xFF9b9b9b),
-                                controller: passwordController,
-                                keyboardType: TextInputType.text,
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                  prefixIcon: Icon(
-                                    Icons.vpn_key,
-                                    color: Colors.grey,
+                                TextField(
+                                  style: TextStyle(color: Color(0xFF000000)),
+                                  cursorColor: Color(0xFF9b9b9b),
+                                  controller: passwordController,
+                                  keyboardType: TextInputType.text,
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    prefixIcon: Icon(
+                                      Icons.vpn_key,
+                                      color: Colors.grey,
+                                    ),
+                                    hintText: "Password",
+                                    hintStyle: TextStyle(
+                                        color: Color(0xFF9b9b9b),
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal),
                                   ),
-                                  hintText: "Password",
-                                  hintStyle: TextStyle(
-                                      color: Color(0xFF9b9b9b),
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.normal),
                                 ),
-                              ),
-                              TextField(
-                                style: TextStyle(color: Color(0xFF000000)),
-                                controller: phoneController,
-                                cursorColor: Color(0xFF9b9b9b),
-                                keyboardType: TextInputType.text,
-                                decoration: InputDecoration(
-                                  prefixIcon: Icon(
-                                    Icons.mobile_screen_share,
-                                    color: Colors.grey,
+                                TextField(
+                                  style: TextStyle(color: Color(0xFF000000)),
+                                  controller: phoneController,
+                                  cursorColor: Color(0xFF9b9b9b),
+                                  keyboardType: TextInputType.text,
+                                  decoration: InputDecoration(
+                                    prefixIcon: Icon(
+                                      Icons.mobile_screen_share,
+                                      color: Colors.grey,
+                                    ),
+                                    hintText: "Phone",
+                                    hintStyle: TextStyle(
+                                        color: Color(0xFF9b9b9b),
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal),
                                   ),
-                                  hintText: "Phone",
-                                  hintStyle: TextStyle(
-                                      color: Color(0xFF9b9b9b),
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.normal),
                                 ),
-                              ),
 
-                              /////////////// SignUp Button ////////////
+                                /////////////// SignUp Button ////////////
 
-                              Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: FlatButton(
-                                    child: Padding(
-                                      padding: EdgeInsets.only(
-                                          top: 8, bottom: 8, left: 10, right: 10),
-                                      child: Text(
-                                        _isLoading ? 'Creating...' : 'Create account',
-                                        textDirection: TextDirection.ltr,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 15.0,
-                                          decoration: TextDecoration.none,
-                                          fontWeight: FontWeight.normal,
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: FlatButton(
+                                      child: Padding(
+                                        padding: EdgeInsets.only(
+                                            top: 8, bottom: 8, left: 10, right: 10),
+                                        child: Text(
+                                          _isLoading ? 'Creating...' : 'Create account',
+                                          textDirection: TextDirection.ltr,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 15.0,
+                                            decoration: TextDecoration.none,
+                                            fontWeight: FontWeight.normal,
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    color: Colors.red,
-                                    disabledColor: Colors.grey,
-                                    shape: new RoundedRectangleBorder(
-                                        borderRadius:
-                                        new BorderRadius.circular(20.0)),
-                                    onPressed: _isLoading ? null :  _handleLogin
+                                      color: Colors.red,
+                                      disabledColor: Colors.grey,
+                                      shape: new RoundedRectangleBorder(
+                                          borderRadius:
+                                          new BorderRadius.circular(20.0)),
+                                      onPressed: _isLoading ? null :  _handleLogin
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-
-                      /////////////// already have an account ////////////
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20),
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                new MaterialPageRoute(
-                                    builder: (context) => LogIn()));
-                          },
-                          child: Text(
-                            'Already have an Account',
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15.0,
-                              decoration: TextDecoration.none,
-                              fontWeight: FontWeight.normal,
+                              ],
                             ),
                           ),
                         ),
-                      ),
-                    ],
+
+                        /////////////// already have an account ////////////
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    new MaterialPageRoute(
+                                        builder: (context) => LogIn()));
+                              },
+                              child: Text(
+                                'Already have an Account',
+                                textDirection: TextDirection.ltr,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15.0,
+                                  decoration: TextDecoration.none,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               )
